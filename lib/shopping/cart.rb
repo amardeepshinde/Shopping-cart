bf0d7ad57f2b99d44f08ba26142b56baccc46037
amardeep shinde <amardeep.shinde2@gmail.com>
2020-04-08 18:01:30 +0530

    Display Method Added

--- lib/shopping/cart.rb
+++ lib/shopping/cart.rb
@@ -3,16 +3,47 @@ require "shopping/cart/version"
 module Shopping
   module Cart
     class Error < StandardError; end
-    # Your code goes here...
+   
+
+    class ShopingCart
+     
+      @@products = { Laptop: 19000 , Mobile: 45000 , Sound: 12000, Tv: 100500 }
+      
+      @@store = []
+
+      def initialize(name,price)
+        @name = name
+        @price = price
+        
+      end
+       
+       
+        def showAllProduct
+           
+            @@products.each do | item , price |
+              puts "#{item}           #{price}"
+              end
+        end
+        
+        
+        
+
+      end
+
+cart1 = Item.new("HDMI",2000)
+cart1.showAllProduct
+
+    
+
+
+
+
+
+
+
   end
 end
 
 
 
 
-class cart
-def mymethod
-
-
-end
-end
